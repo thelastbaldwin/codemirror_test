651ee4ee25cda1f0a97a1975857fd2011b798afb
Steve Minor <steve.e.minor@nordstrom.com>
2016-10-26 12:01:33 -0700

    preview works, but many styling bugs

--- js/main.js
+++ js/main.js
@@ -1,18 +1,51 @@
-var delay;
-// Initialize CodeMirror editor with a nice html5 canvas demo.
-var editor = CodeMirror.fromTextArea(document.getElementById('code'), {
-  mode: 'text/html'
-});
-editor.on("change", function() {
-  clearTimeout(delay);
-  delay = setTimeout(updatePreview, 300);
-});
+window.addEventListener("load", function(){
 
-function updatePreview() {
-  var previewFrame = document.getElementById('preview');
-  var preview =  previewFrame.contentDocument ||  previewFrame.contentWindow.document;
-  preview.open();
-  preview.write(editor.getValue());
-  preview.close();
-}
-setTimeout(updatePreview, 300);
+    var delay;
+    var HTMLEditor = CodeMirror.fromTextArea(document.getElementById("html-code"), {
+      mode: "text/html"
+    });
+
+    var CSSEditor = CodeMirror.fromTextArea(document.getElementById("css-code"), {
+        mode: "css"
+    });
+
+    var JSEditor = CodeMirror.fromTextArea(document.getElementById("js-code"), {
+        mode: "javascript"
+    });
+
+    function updatePreview() {
+      var previewFrame = document.getElementById("preview");
+      var preview =  previewFrame.contentDocument ||  previewFrame.contentWindow.document;
+
+      var newDocument = "<!doctype html>" +
+      "<head><style>" + CSSEditor.getValue() + "</style>" +
+      "</head>" +
+      "<body>" + HTMLEditor.getValue() +
+      "<script>" + JSEditor.getValue() +
+      "</body>";
+
+
+      preview.open();
+      preview.write(newDocument);
+      preview.close();
+    }
+
+    function update(){
+      clearTimeout(delay);
+      delay = setTimeout(updatePreview.call(window), 300);
+    }
+
+    HTMLEditor.on("change", function() {
+      update();
+    });
+
+    CSSEditor.on("change", function() {
+      update();
+    });
+
+    JSEditor.on("change", function() {
+      update();
+    });
+
+    setTimeout(updatePreview, 300);
+});
